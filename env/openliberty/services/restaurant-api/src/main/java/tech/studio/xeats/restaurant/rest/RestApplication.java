2029dac13a5db504ad53fb81abac9bb31e0816f6
Valdir Junior <junior_vs@outlook.com>
2022-06-02 16:43:12 -0300

    setup projeto restaurant

---
+++ env/openliberty/services/restaurant-api/src/main/java/tech/studio/xeats/restaurant/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package tech.studio.xeats.restaurant.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
